b4be698e5c783f5c6c5ff3728f5e287d04f4cac6
Jarold Wong <cwjwong@ucdavis.edu>
2021-04-28 10:31:58 -0700

    remove debugger;

--- src/components/JobFunction.js
+++ src/components/JobFunction.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const JobFunction = (props) => {
-  debugger;
   const percentValues = [100, 90, 80, 70, 60, 50, 40, 30, 20, 10];
   const { comments, description, id, percentage } = props.jobFunction;
 
