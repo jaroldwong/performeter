a97f4fb52e48d0db791db06ce50f12d2eefb37b1
Jarold Wong <cwjwong@ucdavis.edu>
2021-04-28 10:32:12 -0700

    add href to a tag

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -4,18 +4,18 @@ const Tabs = ({ activeNav, handleNav }) => {
   return (
     <div className="tabs">
       <ul>
-        <li className={activeNav === 'Job Functions' && 'is-active'}>
-          <a role="button" onClick={handleNav}>
+        <li className={activeNav === 'Job Functions' ? 'is-active' : null}>
+          <a href="#JobFunctions" onClick={handleNav}>
             Job Functions
           </a>
         </li>
-        <li className={activeNav === 'Achievements' && 'is-active'}>
-          <a role="button" onClick={handleNav}>
+        <li className={activeNav === 'Achievements' ? 'is-active' : null}>
+          <a href="#Achievements" onClick={handleNav}>
             Achievements
           </a>
         </li>
-        <li className={activeNav === 'Future Goals' && 'is-active'}>
-          <a role="button" onClick={handleNav}>
+        <li className={activeNav === 'Future Goals' ? 'is-active' : null}>
+          <a href="#Goals" onClick={handleNav}>
             Future Goals
           </a>
         </li>
