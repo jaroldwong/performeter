1476f58fc2b78b40e852ac3151422f21ecd2d4d9
Jarold Wong <cwjwong@ucdavis.edu>
2021-05-12 15:40:44 -0700

    adding an app context

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 import Header from './components/Header';
 import Tabs from './components/Tabs';
 import CompetencyCounter from './components/CompetencyCounter';
@@ -9,10 +9,13 @@ import Achievements from './components/Achievements';
 import Goals from './components/Goals';
 
 import jobFunctionReducer from './reducers/jobFunctionReducer';
+import AppContextProvider, { AppContext } from './contexts/AppContext';
 
 function App() {
   const initialState =
     JSON.parse(window.localStorage.getItem('performeter')) || {};
+
+  const { appState, appDispatch } = useContext(AppContext);
 
   const [state, dispatch] = useReducer(
     jobFunctionReducer,
@@ -26,19 +29,15 @@ function App() {
     initialState.achievements || ''
   );
   const [goals, setGoals] = useState(initialState.goals || []);
-  const [activeTab, setactiveTab] = useState(
-    initialState.activeTab || 'Job Functions'
-  );
 
   useEffect(() => {
     const data = {
       jobFunctions: state,
       achievements,
       goals,
-      activeTab,
     };
     window.localStorage.setItem('performeter', JSON.stringify(data));
-  }, [state, achievements, goals, activeTab]); // only activate when these values change
+  }, [state, achievements, goals]); // only activate when these values change
 
   // Achievements Handlers
   const updateAchievements = (event) => {
@@ -68,83 +67,85 @@ function App() {
   };
 
   const updateNav = (e) => {
-    setactiveTab(e.target.innerText);
+    appDispatch({ type: 'SELECT_TAB', payload: { tab: e.target.innerText } });
   };
 
   const resetData = () => {
     setJobFunctions([]);
     setAchievements('');
     setGoals([]);
-    setactiveTab('Job Functions');
+    // setactiveTab('Job Functions');
   };
 
   const allComments = state.map((jobFunction) => jobFunction.comments).flat();
 
   return (
-    <div className="App">
-      <Header state={jobFunctions} resetData={resetData} />
-      <section className="section">
-        <div className="columns">
-          <div className="column is-one-quarter">
-            <CompetencyCounter comments={allComments} />
-          </div>
-          <div className="column is-three-quarters">
-            <Tabs activeTab={activeTab} updateNav={updateNav} />
+    <AppContextProvider>
+      <div className="App">
+        <Header state={jobFunctions} resetData={resetData} />
+        <section className="section">
+          <div className="columns">
+            <div className="column is-one-quarter">
+              <CompetencyCounter comments={allComments} />
+            </div>
+            <div className="column is-three-quarters">
+              <Tabs activeTab={appState.activeTab} updateNav={updateNav} />
 
-            {
               {
-                'Job Functions': (
-                  <JobFunctions
-                    jobFunctions={state}
-                    addJobFunction={() =>
-                      dispatch({ type: 'ADD_JOB_FUNCTION' })
-                    }
-                    updateJobFunction={(id, event) =>
-                      dispatch({
-                        type: 'UPDATE_JOB_FUNCTION',
-                        payload: { id, event },
-                      })
-                    }
-                    addComment={(id) =>
-                      dispatch({ type: 'ADD_COMMENT', payload: { id } })
-                    }
-                    updateComment={(
-                      jobFunctionId,
-                      commentIndex,
-                      newComment
-                    ) => {
-                      dispatch({
-                        type: 'UPDATE_COMMENT',
-                        payload: { jobFunctionId, commentIndex, newComment },
-                      });
-                    }}
-                    deleteComment={(jobFunction, commentIndex) =>
-                      dispatch({
-                        type: 'DELETE_COMMENT',
-                        payload: { jobFunction, commentIndex },
-                      })
-                    }
-                  />
-                ),
-                Achievements: (
-                  <Achievements
-                    achievements={achievements}
-                    updateAchievements={updateAchievements}
-                  />
-                ),
-                'Future Goals': (
-                  <Goals
-                    goals={goals}
-                    addGoal={addGoal}
-                    updateGoal={updateGoal}
-                  />
-                ),
-              }[activeTab]
-            }
+                {
+                  'Job Functions': (
+                    <JobFunctions
+                      jobFunctions={state}
+                      addJobFunction={() =>
+                        dispatch({ type: 'ADD_JOB_FUNCTION' })
+                      }
+                      updateJobFunction={(id, event) =>
+                        dispatch({
+                          type: 'UPDATE_JOB_FUNCTION',
+                          payload: { id, event },
+                        })
+                      }
+                      addComment={(id) =>
+                        dispatch({ type: 'ADD_COMMENT', payload: { id } })
+                      }
+                      updateComment={(
+                        jobFunctionId,
+                        commentIndex,
+                        newComment
+                      ) => {
+                        dispatch({
+                          type: 'UPDATE_COMMENT',
+                          payload: { jobFunctionId, commentIndex, newComment },
+                        });
+                      }}
+                      deleteComment={(jobFunction, commentIndex) =>
+                        dispatch({
+                          type: 'DELETE_COMMENT',
+                          payload: { jobFunction, commentIndex },
+                        })
+                      }
+                    />
+                  ),
+                  Achievements: (
+                    <Achievements
+                      achievements={achievements}
+                      updateAchievements={updateAchievements}
+                    />
+                  ),
+                  'Future Goals': (
+                    <Goals
+                      goals={goals}
+                      addGoal={addGoal}
+                      updateGoal={updateGoal}
+                    />
+                  ),
+                }[appState.activeTab]
+              }
+            </div>
           </div>
-        </div>
-      </section>
-    </div>
+        </section>
+      </div>
+    </AppContextProvider>
   );
 }
 
---
+++ src/contexts/AppContext.js
@@ -0,0 +1,35 @@
+import React, { createContext, useEffect, useReducer } from 'react';
+
+export const AppContext = createContext();
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'SELECT_TAB':
+      return { ...state, activeTab: action.payload.tab };
+    default:
+      return state;
+  }
+}
+
+const localData = localStorage.getItem('performeter-app-state');
+const initialAppState = localData
+  ? JSON.parse(localData)
+  : {
+      activeTab: 'Job Functions',
+    };
+
+const AppContextProvider = (props) => {
+  const [appState, appDispatch] = useReducer(reducer, initialAppState);
+
+  useEffect(() => {
+    localStorage.setItem('performeter-app-state', JSON.stringify(appState));
+  }, [appState]);
+
+  return (
+    <AppContext.Provider value={{ appState, appDispatch }}>
+      {props.children}
+    </AppContext.Provider>
+  );
+};
+
+export default AppContextProvider;
