ac262822e9a04045ea629cefd7bcacfaa94a942a
Jarold Wong <cwjwong@ucdavis.edu>
2021-05-12 15:49:09 -0700

    fix AppContext

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Achievements from './components/Achievements';
 import Goals from './components/Goals';
 
 import jobFunctionReducer from './reducers/jobFunctionReducer';
-import AppContextProvider, { AppContext } from './contexts/AppContext';
+import { AppContext } from './contexts/AppContext';
 
 function App() {
   const initialState =
@@ -80,72 +80,70 @@ function App() {
   const allComments = state.map((jobFunction) => jobFunction.comments).flat();
 
   return (
-    <AppContextProvider>
-      <div className="App">
-        <Header state={jobFunctions} resetData={resetData} />
-        <section className="section">
-          <div className="columns">
-            <div className="column is-one-quarter">
-              <CompetencyCounter comments={allComments} />
-            </div>
-            <div className="column is-three-quarters">
-              <Tabs activeTab={appState.activeTab} updateNav={updateNav} />
-
-              {
-                {
-                  'Job Functions': (
-                    <JobFunctions
-                      jobFunctions={state}
-                      addJobFunction={() =>
-                        dispatch({ type: 'ADD_JOB_FUNCTION' })
-                      }
-                      updateJobFunction={(id, event) =>
-                        dispatch({
-                          type: 'UPDATE_JOB_FUNCTION',
-                          payload: { id, event },
-                        })
-                      }
-                      addComment={(id) =>
-                        dispatch({ type: 'ADD_COMMENT', payload: { id } })
-                      }
-                      updateComment={(
-                        jobFunctionId,
-                        commentIndex,
-                        newComment
-                      ) => {
-                        dispatch({
-                          type: 'UPDATE_COMMENT',
-                          payload: { jobFunctionId, commentIndex, newComment },
-                        });
-                      }}
-                      deleteComment={(jobFunction, commentIndex) =>
-                        dispatch({
-                          type: 'DELETE_COMMENT',
-                          payload: { jobFunction, commentIndex },
-                        })
-                      }
-                    />
-                  ),
-                  Achievements: (
-                    <Achievements
-                      achievements={achievements}
-                      updateAchievements={updateAchievements}
-                    />
-                  ),
-                  'Future Goals': (
-                    <Goals
-                      goals={goals}
-                      addGoal={addGoal}
-                      updateGoal={updateGoal}
-                    />
-                  ),
-                }[appState.activeTab]
-              }
-            </div>
+    <div className="App">
+      <Header state={jobFunctions} resetData={resetData} />
+      <section className="section">
+        <div className="columns">
+          <div className="column is-one-quarter">
+            <CompetencyCounter comments={allComments} />
           </div>
-        </section>
-      </div>
-    </AppContextProvider>
+          <div className="column is-three-quarters">
+            <Tabs activeTab={appState.activeTab} updateNav={updateNav} />
+
+            {
+              {
+                'Job Functions': (
+                  <JobFunctions
+                    jobFunctions={state}
+                    addJobFunction={() =>
+                      dispatch({ type: 'ADD_JOB_FUNCTION' })
+                    }
+                    updateJobFunction={(id, event) =>
+                      dispatch({
+                        type: 'UPDATE_JOB_FUNCTION',
+                        payload: { id, event },
+                      })
+                    }
+                    addComment={(id) =>
+                      dispatch({ type: 'ADD_COMMENT', payload: { id } })
+                    }
+                    updateComment={(
+                      jobFunctionId,
+                      commentIndex,
+                      newComment
+                    ) => {
+                      dispatch({
+                        type: 'UPDATE_COMMENT',
+                        payload: { jobFunctionId, commentIndex, newComment },
+                      });
+                    }}
+                    deleteComment={(jobFunction, commentIndex) =>
+                      dispatch({
+                        type: 'DELETE_COMMENT',
+                        payload: { jobFunction, commentIndex },
+                      })
+                    }
+                  />
+                ),
+                Achievements: (
+                  <Achievements
+                    achievements={achievements}
+                    updateAchievements={updateAchievements}
+                  />
+                ),
+                'Future Goals': (
+                  <Goals
+                    goals={goals}
+                    addGoal={addGoal}
+                    updateGoal={updateGoal}
+                  />
+                ),
+              }[appState.activeTab]
+            }
+          </div>
+        </div>
+      </section>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -5,9 +5,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import AppContextProvider from './contexts/AppContext';
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
