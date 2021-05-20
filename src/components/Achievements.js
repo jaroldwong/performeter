736dd70b78fda7fa9e598b72616225630ab10ca4
Jarold Wong <cwjwong@ucdavis.edu>
2021-05-20 14:29:54 -0700

    testing react bulma

--- src/components/Achievements.js
+++ src/components/Achievements.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Box, Form, Heading } from 'react-bulma-components';
 
 const Achievements = ({ achievements, updateAchievements }) => {
   return (
     <>
-      <h1 className="title">Achievements</h1>
-      <div className="box">
-        <div className="field">
-          <label className="label">Achievements</label>
-          <div className="control">
-            <textarea
-              name="achievement"
-              className="textarea"
+      <Heading size={3}>Achievements</Heading>
+      <Box>
+        <Form.Field>
+          <Form.Label>Achievements</Form.Label>
+          <Form.Control>
+            <Form.Textarea
               placeholder="Address your goals from last year"
               value={achievements}
               onChange={updateAchievements}
-            ></textarea>
-          </div>
-        </div>
-      </div>
+            />
+          </Form.Control>
+        </Form.Field>
+      </Box>
     </>
   );
 };
