e2afdb7865284c00fd04d5131d17aa039a4567e0
Jarold Wong <cwjwong@ucdavis.edu>
2022-05-18 14:17:25 -0700

    update core competencies for 2021

---
+++ src/utils/constants.js
@@ -0,0 +1,73 @@
+export const CORE_COMPETENCIES = [
+  'Continuous Improvement',
+  'Continuous Learning',
+  'Problem Solving',
+  'Service Focus',
+  'Belonging and Community',
+  'Collaboration',
+  'Communication',
+  'Change Agility',
+  'Mission and Vision Focus',
+  'Stewardship'
+];
+
+export const BEHAVIORAL_INDICATORS = {
+  'Continuous Improvement': [
+    'adopted ideas, technology, and practices that increase efficiency, eliminate waste, and improve performance',
+    'took initiative to make improvements to current work',
+    'used data, feedback, and input from others to improve the quality of products and services',
+  ],
+  'Continuous Learning': [
+    'took responsibility for development',
+    'maintained fluency in appropriate work applications, software, or tools',
+    'kept up-to-date on current research, trends, and technology',
+    'identified and pursued areas for development and training that will enhance job performance',
+    'sought coaching and feedback to increase self-awareness and personal growth'
+  ],
+  'Problem Solving': [
+    'evaluated the pros and cons, risks and benefits of different solutions',
+    'prioritized opportunities and challenges in order to solve urgent, high impact problems',
+    'worked effectiviely with others to solve problems and make decisions',
+    'sought input from partners',
+  ],
+  'Service Focus': [
+    'sought and used feedback from a variety of sources to improve service quality',
+    'ensured that stakeholder needs are fulfilled',
+    'honored commitments and kept promises',
+    'adapted service delivery to meet the needs of diverse client base'
+  ],
+  Collaboration: [
+    'cooperated with others',
+    'shared information and knowledge to identify and implement solutions in which all parties can benefit',
+    'helped and supported fwllo employees in their work to contribute to overall success',
+    'placed the needs of the team above self-interest',
+    'built and maintained effective working relationships'
+  ],
+  'Communication': [
+    'consistently shared accurate, timely information',
+    'took into account audience and communication method',
+    'stated opinions honestly and in a straight-forward manner',
+    'maintained open dialogue with others',
+    'actively listened',
+  ],
+  'Change Agility': [
+    'adapted approaches as needed to address changing priorities',
+    'was flexible, open, and receiptive to new approaches',
+    'stepped outside of comfort zone',
+    'sought to understand context while navigating through organizational change',
+    'recognized responses to change and responed in a way that is productive for the organization and team members'
+  ],
+  'Mission and Vision Focus': [
+    "used the organization's mission and vision as guideposts for decision-making",
+    'connected individual role and aligned goals in fulfillment of the UC mission and vision',
+    'inspired others to achieve the mission and vision of the organization and the University'
+  ],
+  'Stewardship': [
+    'acted in a manner consistent with the UC Standrads of Ethical Conduct and other UC policies',
+    'utilized available resources to achieve organizational goals',
+    'modeld accountability',
+    'understood and adhered to safety guidelines',
+    'reported and corrected potential threats',
+    'modeled safe behaviors'
+  ]
+};
